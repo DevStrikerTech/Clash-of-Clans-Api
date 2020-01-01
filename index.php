bbcc78891b224f95793fb893067880aee2e9ef98
KingCobra2018 <kcobra557@gmail.com>
2020-01-01 15:39:05 +0000

    Adding base code

--- index.php
+++ index.php
@@ -2,7 +2,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title></title>
+    <?php
+        $clantag = "<CLANTAG>";
+
+        $token = "<APITOKEN>";
+
+        $url = "https://api.clashofclans.com/v1/clans/" . urlencode($clantag);
+
+        $ch = curl_init($url);
+
+        $headr = array();
+        $headr[] = "Accept: application/json";
+        $headr[] = "Authorization: Bearer ".$token;
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $res = curl_exec($ch);
+        $data = json_decode($res, true);
+        curl_close($ch);
+
+        if (isset($data["reason"])) {
+        $errormsg = true;
+        }
+
+        $members = $data["memberList"];
+
+        ?>
+    <title><?php echo $data["name"]; ?></title>
 </head>
 <body>
 
